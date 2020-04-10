1cff5711fd600777f066040aaf752e7c774bd96c
Jesper Sørensen <jeppester1@gmail.com>
2020-04-10 21:38:41 +0200

    Keep track of hidden nodes
    This makes it possible to change settings on the fly

--- js/index.js
+++ js/index.js
@@ -9,43 +9,83 @@ const settingsKeys = [
 
 const articleFilter = {
   settings: null,
+  hiddenElements: [],
+  checkedNodes: 0,
 
   init() {
-    ['blockKeywords', 'checkSelector', 'update', 'getAreaIsTooLarge'].forEach(fn => this[fn] = this[fn].bind(this))
-
     chrome.storage.sync.get(settingsKeys, settings => {
-      if (!settings.enabled) return
-
       this.settings = settings
       this.settings.keywords = settings.keywords.trim().split(/\s*\n\s*/)
       this.settings.selectors = settings.selectors.trim().split(/\s*\n\s*/)
 
+      if (!this.settings.enabled) return
 
-      this.observer = new MutationObserver(this.update)
-      this.observer.observe(document.body, { childList: true })
-      this.update()
+      this.observer = new MutationObserver(this.handleMutation)
+      this.observer.observe(document.body, { subtree: true, childList: true })
+      this.updateNode(document.body)
     })
   },
 
   blockKeywords(element) {
+    this.checkedNodes++
+
     var innerText = element.innerText.toLowerCase()
-    for (let keyword of this.settings.keywords) {
-      if (innerText.length > this.settings.maxLength && this.getAreaIsTooLarge(element)) {
-        continue
-      }
-      if (innerText.indexOf(keyword) !== -1) {
-        switch (this.settings.removalStrategy) {
-          case 'hide': {
-            element.style.display = "none"
-            break;
-          }
-          case 'makeInvisible': {
-            element.style.visibility = 'hidden'
-          }
+    if (innerText.length < this.settings.maxLength || !this.getAreaIsTooLarge(element)) {
+      for (let keyword of this.settings.keywords) {
+        if (innerText.indexOf(keyword) !== -1) {
+          this.updateElementVisible(element, false)
+          return
         }
-        break
       }
     }
+  },
+
+  updateElementVisible(element, shouldBeVisible) {
+    const isCurrentlyVisible = !this.hiddenElements.includes(element)
+
+    if (isCurrentlyVisible) {
+      if (!shouldBeVisible) this.hideElement(element)
+    }
+    else {
+      if (shouldBeVisible) this.showElement(element)
+    }
+  },
+
+  hideElement(element) {
+    this.hiddenElements.push(element)
+
+    switch (this.settings.removalStrategy) {
+      case 'hide': {
+        element._previousDisplay = element.style.display
+        element.style.display = "none"
+        break;
+      }
+      case 'makeInvisible': {
+        element._previousVisibility = element.style.visibility
+        element.style.visibility = 'hidden'
+        break;
+      }
+    }
+  },
+
+  reset() {
+    switch (this.settings.removalStrategy) {
+      case 'hide': {
+        this.hiddenElements.forEach(element => {
+          element.style.display = element._previousDisplay
+        })
+        break;
+      }
+      case 'makeInvisible': {
+        this.hiddenElements.forEach(element => {
+          element.style.visibility = element._previousVisibility
+        })
+        break;
+      }
+    }
+
+    this.checkedNodes = 0
+    this.hiddenElements = []
   },
 
   getAreaIsTooLarge(element) {
@@ -53,15 +93,45 @@ const articleFilter = {
     return this.settings.maxAreaPx < area
   },
 
-  checkSelector(selector) {
-    elements = document.body.querySelectorAll(selector)
-    elements.forEach(this.blockKeywords)
+  eleminateNodesInNodes(nodes) {
+    const remainingToCheck = Array.from(nodes)
+    const accepted = []
+
+    let node
+    while (node = remainingToCheck.pop()) {
+      if (accepted.some(otherNode => otherNode.contains(node))) continue
+      if (remainingToCheck.some(otherNode => otherNode.contains(node))) continue
+      if (this.hiddenElements.some(otherNode => otherNode.contains(node))) continue
+      accepted.push(node)
+    }
+
+    return accepted
   },
 
-  update() {
-    this.settings.selectors.forEach(this.checkSelector)
+  checkContainer(node) {
+    const matchingNodes = node.querySelectorAll(this.settings.selectors.join(','))
+    const sanitizedMatchingNodes = this.eleminateNodesInNodes(matchingNodes)
+
+    sanitizedMatchingNodes.forEach(this.blockKeywords)
+  },
+
+  updateNode(node) {
+    this.checkContainer(node)
+  },
+
+  handleMutation(mutationRecords) {
+    mutationRecords.forEach(({ target }) => this.updateNode(target))
+  },
+
+  handleSettingsChange() {
+    this.reset()
+    this.init()
   }
 }
 
+Object.getOwnPropertyNames(articleFilter).forEach(name => {
+  if (typeof articleFilter[name] === 'function') articleFilter[name] = articleFilter[name].bind(articleFilter)
+})
 articleFilter.init()
 
+chrome.storage.onChanged.addListener(articleFilter.handleSettingsChange)
